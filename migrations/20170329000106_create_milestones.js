4c3c2731d2bf8022461bb7242e47c37a0a40f8fe
sadooghi <gh.sadooghi@gmail.com>
2017-03-29 01:11:20 +0000

    add a column to the created table

--- migrations/20170329000106_create_milestones.js
+++ migrations/20170329000106_create_milestones.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return Promise.all([
     knex.schema.createTable('milestones', function(table){
-      table.integer('id');
+      table.integer('id').notNull();
       table.string('description');
       table.date('date_achieved');
     })
